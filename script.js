ee32697bd144e48078848932821169d86ed33392
Ekhi <117848909+EkhiABB@users.noreply.github.com>
2023-09-21 16:26:49 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+let ax=0;
+let ay=0;
+let az=0;
+
+window.addEventListener("load", async function () {
+
+if (window.DeviceMotionEvent != undefined) {
+	window.ondevicemotion = function(e) {
+		ax = e.acceleration.x;
+		ay = e.acceleration.y;
+		az = e.acceleration.z;
+		document.getElementById("accelerationX").innerHTML = ax;
+		document.getElementById("accelerationY").innerHTML = ay;
+		document.getElementById("accelerationZ").innerHTML = az;
+	}
+}
+
+}
+	
